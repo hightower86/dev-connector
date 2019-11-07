682d0209e1d40ca8740443af53976ec5cb2f37bf
Adel <htwr86@gmail.com>
2019-11-07 20:15:39 +0300

    config files and connect to mongo db

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoUri');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log('MongoDB connected...');
+  } catch (error) {
+    console.error(error.message);
+    // Exti process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
