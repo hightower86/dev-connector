b9db59a694d12847338f363b4c79e7153756a7ac
Adel <htwr86@gmail.com>
2019-11-08 20:06:45 +0300

    express-validator in user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,28 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
 
-// @route    GET api/users
-// @descr    Test route
+// @route    POST api/users
+// @descr    Register user
 // @access   Public
-router.get('/', (req, res) => res.send('users api'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Password should be at list 6 characters length'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
