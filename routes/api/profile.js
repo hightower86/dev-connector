1e4ac4eee3a36ec03c5363adbcd7a7ea634fdea3
Adel <htwr86@gmail.com>
2019-11-13 23:02:41 +0300

    get users profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
+const auth = require('../../middlware/auth.js');
 
-// @route    GET api/profile
-// @descr    Test route
-// @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../model/Profile');
 
+// @route    GET api/profile/me
+// @descr    Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error!');
+  }
+});
 module.exports = router;
