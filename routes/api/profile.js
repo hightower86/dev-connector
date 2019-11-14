547787b58eeed9ff596399501ad3e5e6f27192e5
Adel <htwr86@gmail.com>
2019-11-14 15:09:45 +0300

    17.4 add POST request to profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const auth = require('../../middlware/auth.js');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../model/Profile');
 
@@ -20,4 +21,42 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server error!');
   }
 });
+
+// @route    POST api/profile/me
+// @descr    Create / update user profile
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Status is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ errors: errors.array() });
+    }
+    const {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      githubusername,
+      experience,
+      education,
+      social,
+      date
+    } = req.body;
+    res.status(200).send('OK');
+  }
+);
 module.exports = router;
