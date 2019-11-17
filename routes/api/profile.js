b8d77d0c7c3d21e68d0bf0ada033a04896ee6c02
Adel <htwr86@gmail.com>
2019-11-17 14:41:14 +0300

    22.5 Add Profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -186,7 +186,7 @@ router.put(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).send({ errors: errors.array() });
     }
 
     const {
@@ -247,5 +247,68 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route    PUT api/profile/education
+// @descr    Add new education to profile
+// @access   Private
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required')
+        .not()
+        .isEmpty(),
+      check('degree', 'Degree is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty(),
+      check('fieldofstudy', 'Field of stuy is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ errors: errors.array() });
+    }
+
+    const profile = await Profile.findOne({ user: req.user.id });
+    if (!profile) {
+      return res.status(400).send('Profile not found in db');
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      descriprion
+    } = req.body;
+
+    const newEducation = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      descriprion
+    };
+
+    try {
+      profile.education.unshift(newEducation);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
