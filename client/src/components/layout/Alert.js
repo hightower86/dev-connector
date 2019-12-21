4f135a6b8da8a539bf4c28643c37268e68359213
Adel <htwr86@gmail.com>
2019-12-22 00:42:05 +0300

    39. remove alert with timeout and click

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { removeAlert } from '../../actions/alert';
 
-const Alert = ({ alerts }) =>
+const Alert = ({ alerts, removeAlert }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map(({ msg, alertType, id }) => (
@@ -11,7 +12,7 @@ const Alert = ({ alerts }) =>
       role='alert'
       key={id}
     >
-      <strong className='font-bold'>Attention!</strong>
+      {/* <strong className='font-bold'>Attention!</strong> */}
       <span className='block sm:inline'>{msg}</span>
       <span className='absolute top-0 bottom-0 right-0 px-4 py-3'>
         <svg
@@ -19,6 +20,7 @@ const Alert = ({ alerts }) =>
           role='button'
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 20 20'
+          onClick={() => removeAlert(id)}
         >
           <title>Close</title>
           <path d='M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z' />
@@ -32,4 +34,4 @@ const mapStateToProps = state => ({
   alerts: state.alert
 });
 
-export default connect(mapStateToProps)(Alert);
+export default connect(mapStateToProps, { removeAlert })(Alert);
