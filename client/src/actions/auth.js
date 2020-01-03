2a8c3823ba36734cf65076f1b349f91bdbeda649
Adel <htwr86@gmail.com>
2020-01-03 20:54:55 +0300

    43. Logout and nav links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 
 import { setAlert } from '../actions/alert';
@@ -82,4 +83,9 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  //const propTypes = PropTypes();
+  const authLinks = (
+    <ul className=' flex items-center '>
+      <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
+        <Link to='!#' onClick={logout}>
+          Logout
+        </Link>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul className=' flex items-center '>
+      <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
+        <Link to='!#'>Developers</Link>
+      </li>
+      <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
   return (
     <div className='w-full p-4 bg-gray-500'>
       <nav className='flex justify-between'>
         <h2 className=' text-lg'>
           <Link to='/'>Logo</Link>
         </h2>
-        <ul className=' flex items-center '>
-          <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
-            <Link to='!#'>Developers</Link>
-          </li>
-          <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
-            <Link to='/register'>Register</Link>
-          </li>
-          <li className='mr-3 uppercase tracking-wide hover:text-white cursor-pointer'>
-            <Link to='/login'>Login</Link>
-          </li>
-        </ul>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        )}
       </nav>
     </div>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({ auth: state.auth });
+export default connect(mapStateToProps, { logout })(Navbar);
