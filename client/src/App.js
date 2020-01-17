7f08982fae7b724017b0b24e000ce671efc5109c
Adel <htwr86@gmail.com>
2020-01-17 21:27:49 +0300

    47.4 CreateProfile component with route

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
 
@@ -34,6 +35,7 @@ const App = () => {
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <Route exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
