8271a01025a1ee3e0a7ac426d91f80ac22ff8348
Adel <htwr86@gmail.com>
2020-01-29 20:38:16 +0300

    50.2 add experience action added

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,7 +5,8 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  GET_PROFILES
+  GET_PROFILES,
+  UPDATE_PROFILE
 } from '../actions/types';
 
 // Get current users profile
@@ -71,6 +72,32 @@ export const getProfiles = () => async dispatch => {
       payload: res.data
     });
   } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// ADD EXPERIENCE
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const res = await axios.put('api/profile/experience', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience added', 'success'));
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
